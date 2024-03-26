7715fe986747543634f19144467779fc22b983ff
AyanCh0w <github@redbot.pro>
2024-03-26 13:53:53 -0400

    removed font

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 "use client"
-import Image from "next/image"
-import localFont from "next/font/local"
 import Audio from "@/components/Audio"
-
-const supreme = localFont({ src: "../public/fonts/Supreme-Variable.ttf" })
 
 export default function Home() {
   return (
